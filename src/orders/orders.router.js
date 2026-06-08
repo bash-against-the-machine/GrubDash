5a81e28c3809c3fd12abd0d10ccad5db44ae8d34
bash-against-the-machine <davidsvolkov@protonmail.com>
2026-06-08 15:37:44 -0700

    completed orders router, successful completion of all tests

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,5 +1,33 @@
 const router = require("express").Router();
+const controller = require("./orders.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-// TODO: Implement the /orders routes needed to make the tests pass
+// Routes for /orders
+router
+  .route("/")
+  .get(controller.list)
+  .post(
+    controller.bodyDataHas("deliverTo"),
+    controller.bodyDataHas("mobileNumber"),
+    controller.dishesIsValidArray,
+    controller.create
+  )
+  .all(methodNotAllowed);
+
+// Routes for /orders/:orderId
+router
+  .route("/:orderId")
+  .get(controller.orderExists, controller.read)
+  .put(
+    controller.orderExists,
+    controller.bodyDataHas("deliverTo"),
+    controller.bodyDataHas("mobileNumber"),
+    controller.dishesIsValidArray,
+    controller.statusIsValid,
+    controller.idMatchesRoute,
+    controller.update
+  )
+  .delete(controller.orderExists, controller.destroy)
+  .all(methodNotAllowed);
 
 module.exports = router;

080c7026832d2759a18403dd3d0c217c1272f4f4
bash-against-the-machine <davidsvolkov@protonmail.com>
2026-06-08 14:16:30 -0700

    added router for dishes

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,5 +1,35 @@
 const router = require("express").Router();
+const controller = require("./dishes.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-// TODO: Implement the /dishes routes needed to make the tests pass
+// Routes for /dishes
+router
+  .route("/")
+  .get(controller.list)
+  .post(
+    controller.bodyDataHas("name"),
+    controller.bodyDataHas("description"),
+    controller.bodyDataHas("image_url"),
+    controller.bodyDataHas("price"),
+    controller.priceIsValidNumber,
+    controller.create
+  )
+  .all(methodNotAllowed);
+
+// Routes for /dishes/:dishId
+router
+  .route("/:dishId")
+  .get(controller.dishExists, controller.read)
+  .put(
+    controller.dishExists,
+    controller.bodyDataHas("name"),
+    controller.bodyDataHas("description"),
+    controller.bodyDataHas("image_url"),
+    controller.bodyDataHas("price"),
+    controller.priceIsValidNumber,
+    controller.idMatchesRoute,
+    controller.update
+  )
+  .all(methodNotAllowed);
 
 module.exports = router;
